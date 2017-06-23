26198d30bf039ebc9bade344e8bf56484332f63d
rForcee <erwannfrancois@gmail.com>
2017-06-23 23:41:08 +0200

    Ajout d'un joueur en BDD BUG

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def add_player():
 
   if len(players) == 0:
 	  db = Db()
-	  sqlDeletePartie = "DELETE * FROM partie;"
+	  sqlDeletePartie = "DELETE FROM partie;"
 	  sqlInsertPartie = "INSERT INTO partie(p_nom) VALUES('" + "partie" +"');"
 	  sqlInsertMap = "INSERT INTO map(m_centreX, m_centreY, m_coordX, m_coordY, p_id) VALUES(100,100,50,50,(SELECT p_id FROM partie LIMIT 1));"
 	  sqlInsertPlayer = "INSERT INTO joueur(j_pseudo, j_budget, p_id) VALUES('"+ name +"','"+ str(budget) +"', (SELECT p_id FROM partie LIMIT 1));"
