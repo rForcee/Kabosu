6e75b6ce34621cd7897e6de53a2499c5da2746b7
rForcee <erwannfrancois@gmail.com>
2017-06-26 23:45:04 +0200

    Changement dans /players : liste

--- main.py
+++ main.py
@@ -76,9 +76,13 @@ def add_player():
   sqlDrinks = "SELECT b_nom as name, b_prixprod as price, b_alcool as hasAlcohol, b_chaud as isHot FROM boisson WHERE j_id = (SELECT j_id FROM joueur WHERE j_pseudo = '"+name+"');"
   coord = db.select(sqlCoord)[0]
   budgetBase = db.select(sqlBudget)[0]['j_budget']
-  nbSales = db.select(sqlSales)[0]['nbSales']
+  nbSales = db.select(sqlSales)[0]
   drinksInfo = db.select(sqlDrinks)
   db.close()
+  print nbSales
+  print budgetBase
+  print drinksInfo
+  print coord
   profit = budgetBase - budget;
   info = {"cash": budgetBase, "sales": nbSales, "profit": profit, "drinksOffered": drinksInfo}
 
