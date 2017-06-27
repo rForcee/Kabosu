d94757feefb148b22eca575ddc628e2464d9f365
franck merou <franck.merou@gmail.com>
2017-06-27 12:53:25 +0200

    correction route map

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def envoieMapJava():
   infoBoisson = db.select(sqlBoisson)
 
   sqlRank = "SELECT j_pseudo FROM joueur ORDER BY j_budget;"
-  rank = [db.select(sqlRank)[0]['j_pseudo']]
+  rank = db.select(sqlRank)[0]['j_pseudo']
   playerInfo = infoJoueur+infoBoisson
   db.close()
   print rank
