8df0cb1b92dfee072c20f49b32437c661a0add07
franck merou <franck.merou@gmail.com>
2017-06-27 10:36:32 +0200

    debug route map

--- main.py
+++ main.py
@@ -201,11 +201,13 @@ def envoieMapJava():
   db = Db()
   sqlMap = "SELECT * FROM map;"
   infoMap = db.select(sqlMap)
-  sqlJoueur = "SELECT j_id, j_pseudo, j_budget, j_coordX, j_coordY FROM joueur;"
+  sqlJoueur = "SELECT j_id, j_pseudo, j_budget, j_coordX, j_coordY FROM joueur ORDER BY j_budget;"
   infoJoueur = db.select(sqlJoueur)
   sqlBoisson = "SELECT b_id, b_nom, b_alcool, b_chaud, b_prixvente FROM boisson;"
   infoBoisson = db.select(sqlBoisson)
-  info = infoMap+infoJoueur+infoBoisson
+
+  rank = db.select(sqlJoueur)[0]['j_id']['j_pseudo']
+  info = infoMap + infoJoueur + infoBoisson + rank
   db.close()
   return json_response(info)
 
