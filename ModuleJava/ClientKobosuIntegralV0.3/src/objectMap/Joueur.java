1d8ec37200c4921a2ef41bcdcd631fafb796f9fe
franck merou <franck.merou@gmail.com>
2017-06-29 01:21:12 +0200

    update v3.0

--- ModuleJava/ClientKobosuIntegralV0.3/src/objectMap/Joueur.java
+++ ModuleJava/ClientKobosuIntegralV0.3/src/objectMap/Joueur.java
@@ -8,6 +8,7 @@ public class Joueur {
 	private int nbPub;
 	protected DataTramePlayerInfo infojoueur = new DataTramePlayerInfo();
 	protected DataTrameItemJoueur infoItem = new DataTrameItemJoueur();
+	
 	public int getNbPub() {
 		return nbPub;
 	}
@@ -20,5 +21,16 @@ public class Joueur {
 	public void setRang(int rang) {
 		Rang = rang;
 	}
-	
+	public DataTramePlayerInfo getInfojoueur() {
+		return infojoueur;
+	}
+	public void setInfojoueur(DataTramePlayerInfo infojoueur) {
+		this.infojoueur = infojoueur;
+	}
+	public DataTrameItemJoueur getInfoItem() {
+		return infoItem;
+	}
+	public void setInfoItem(DataTrameItemJoueur infoItem) {
+		this.infoItem = infoItem;
+	}
 }
