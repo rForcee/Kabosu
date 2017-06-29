a7836a02f497011ad97b10b0b4e727dbeab09f2d
rForcee <erwannfrancois@gmail.com>
2017-06-29 17:40:57 +0200

    toggle

--- static/js/jeu.js
+++ static/js/jeu.js
@@ -67,17 +67,16 @@ function mapPlayer() {
 				nom = data.playerInfo.drinksOffered[drinks].name;
 				price = data.playerInfo.drinksOffered[drinks].price;
 				var dataContent = "";
-				"<table><thead><tr><th>Ingrédient</th><th>Quantité</th><th>Prix unitaire</th></tr></thead><tbody><tr><td>Eau gazeuse</td><td>1</td><td>0.4</td></tr></tbody></table>"
-				for(ingredient of ingredients)
+				for(ingredient of ingredientsListe)
 				{
 					if(ingredient.b_nom == nom)
 					{
 						dataContent = dataContent + "<tr><td>"+ ingredient.i_nom + "</td><td>" + ingredient.r_qte + "</td><td>" + ingredient.i_prix + "€</td></tr>"
 					}
 				}
-			
+				content = "<table><thead><tr><th>Ingrédient</th><th>Quantité</th><th>Prix unitaire</th></tr></thead><tbody>" + dataContent +"</tbody></table>"
 				var ligne = "<tr id=\"" + nom + "\">"+
-	              "<td class=\"boissonsTD\">"+nom+"</td>"+
+	              "<td class=\"boissonsTD\"><a href=\"#\" title=\"Recette de la " + nom +"\" data-toggle=\"popover\" data-html=\"true\" data-trigger=\"focus\" data-content=\"" + content+ "\" class=\"recette\">"+nom+"</a></td>"+
 	              "<td><input type=\"number\" min=\"0\" name=\""+ nom +"\" class=\"form-control\"></td>" +
 	              "<td><input type=\"text\" name=\""+ nom +"\" class=\"form-control\"></td>" +
 	              "<td>"+ price +"</td>" +
