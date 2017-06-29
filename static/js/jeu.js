70e70d9a9b8340218e5b185655b9a68c0f152a45
rForcee <erwannfrancois@gmail.com>
2017-06-29 19:01:01 +0200

    toggle

--- static/js/jeu.js
+++ static/js/jeu.js
@@ -60,40 +60,52 @@ function mapPlayer() {
 		       	ingredientsListe = ingredients;
 			
 
-			for(drinks in data.playerInfo.drinksOffered)
-			{
-				nom = data.playerInfo.drinksOffered[drinks].name;
-				console.log(nom)
-				price = data.playerInfo.drinksOffered[drinks].price;
-				var dataContent = "";
-				console.log(ingredientsListe)
-				for(ingredient in ingredientsListe)
+				for(drinks in data.playerInfo.drinksOffered)
 				{
-					console.log(ingredient)
-					if(ingredientsListe[ingredient].b_nom == nom)
+					nom = data.playerInfo.drinksOffered[drinks].name;
+					console.log(nom)
+					price = data.playerInfo.drinksOffered[drinks].price;
+					var dataContent = "";
+					console.log(ingredientsListe)
+					for(ingredient in ingredientsListe)
 					{
-						dataContent = dataContent + "<tr><td>"+ ingredientsListe[ingredient].i_nom + "</td><td>" + ingredientsListe[ingredient].r_qte + "</td><td>" + ingredientsListe[ingredient].i_prix + "€</td></tr>"
-						console.log(dataContent)
+						console.log(ingredient)
+						if(ingredientsListe[ingredient].b_nom == nom)
+						{
+							dataContent = dataContent + "<tr><td>"+ ingredientsListe[ingredient].i_nom + "</td><td>" + ingredientsListe[ingredient].r_qte + "</td><td>" + ingredientsListe[ingredient].i_prix + "€</td></tr>"
+							console.log(dataContent)
+						}
 					}
-				}
-				content = "<table><thead><tr><th>Ingrédient</th><th>Quantité</th><th>Prix unitaire</th></tr></thead><tbody>" + dataContent +"</tbody></table>"
+					content = "<table><thead><tr><th>Ingrédient</th><th>Quantité</th><th>Prix unitaire</th></tr></thead><tbody>" + dataContent +"</tbody></table>"
 
-				var ligne = "<tr id=\"" + nom + "\">"+
-	              "<td class=\"boissonsTD\"><a href=\"#\" title=\"Recette : " + nom +"\" data-toggle=\"popover\" data-html=\"true\" data-trigger=\"focus\" data-content=\"" + content+ "\" class=\"recette\">"+nom+"</a></td>"+
-	              "<td><input type=\"number\" min=\"0\" name=\""+ nom +"\" class=\"form-control\"></td>" +
-	              "<td><input type=\"text\" name=\""+ nom +"\" class=\"form-control\"></td>" +
-	              "<td>"+ price +"€</td>" +
-	              "<td></td>" +
-	            "</tr>";
-	            console.log(ligne)
+					var ligne = "<tr id=\"" + nom + "\">"+
+		              "<td class=\"boissonsTD\"><a href=\"#\" title=\"Recette : " + nom +"\" data-toggle=\"popover\" data-html=\"true\" data-trigger=\"focus\" data-content=\"" + content+ "\" class=\"recette\">"+nom+"</a></td>"+
+		              "<td><input type=\"number\" min=\"0\" name=\""+ nom +"\" class=\"form-control prod\"></td>" +
+		              "<td><input type=\"text\" name=\""+ nom +"\" class=\"form-control prixvente\"></td>" +
+		              "<td>"+ price +"€</td>" +
+		              "<td></td>" +
+		            "</tr>";
+		            console.log(ligne)
 
-	            $('#boissons > tbody:last-child').append(ligne);
+		            $('#boissons > tbody:last-child').append(ligne);
 
-        	}
-			$('[data-toggle="popover"]').popover();   
+		    	}
+				$('[data-toggle="popover"]').popover();   
 
         	});
 	});
+}
+
+function getActions() {
+	$.ajax('https://kabosu.herokuapp.com/map/'+ playerName)
+       .done(function(data){
+			for(drinks in data.playerInfo.drinksOffered)
+			{
+				nom = data.playerInfo.drinksOffered[drinks].name;
+				var test = $('#'+nom+' > td > input').value()
+				console.log(test)
+			}
+       	});
 }
 
 function ingredientsPlayer() {
